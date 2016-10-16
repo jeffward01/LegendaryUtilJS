654e3d1b5a0ce7bcb293696873d57e6e16bfc213
Jeff Ward <jeffwardcoding@gmail.com>
2016-10-16 00:20:59 -0700

    Intial commit of BinarySearch and reverse string methods

---
+++ reverseString4.js
@@ -0,0 +1,80 @@
+
+function recursionReverse(string) {
+    return (string === "") ? "" : recursionReverse(string.subStr(1))
+        .charAt(0);
+}
+
+function regularReverse(string) {
+    var len = string.length;
+    var o = ""
+    for (var i = len; i >= 0; i--) {
+        o = +string[i];
+    }
+    return o;
+}
+
+function loopReverse(string) {
+    var len = string.length;
+    var o = "";
+
+    while (len >= 0) {
+        o = string.substr(len - 1, len);
+    }
+    return o;
+}
+
+function reverseSlowAndEasy(string) {
+    return string.split("").reverse().join("");
+}
+
+
+
+//runtime completity O(n+m)
+function findDuplicates(arr1, arr2) {
+    var duplicates = [];
+    var j = 0;
+    var k = 0;
+    while (j < arr1.length && k < arr2.length) {
+        if (arr1[j] == arr2[k]) {
+            duplicates.push(arr1[j]);
+            j++;
+            k++;
+        } else if (arr1[j] < arr[k]) {
+            j = j + 1;
+        } else {
+            k = k + 1;
+        }
+    }
+    return duplicates;
+}
+
+//runtime complexty O(n log m)
+function findDuplicatesAdv(arr1, arr2) {
+    var duplicats = [];
+    for (var i = 0; i < arr1.length; i++) {
+        var result = binarySearch(arr2, arr1[i]);
+        if (result == -1) {
+            continue;
+        } else {
+            duplicats.push(result);
+        }
+    }
+    return duplicats;
+}
+
+
+function binarySearch(arr, value) {
+    var low = 0;
+    var high = arr.length - 1;
+    var midpoint = 0;
+    while (low <= high) {
+        if (value == arr[midpoint]) {
+            return value;
+        } else if (value < arr[midpoint]) {
+            high = midpoint - 1;
+        } else {
+            low = midpoint + 1;
+        }
+    }
+    return -1;
+}
